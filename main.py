930274f03b8a438d112e7c454bb9dde4c294f728
chamc <chamchi0129@gmail.com>
2023-05-25 18:15:48 +0900

    뽑기추가, 가위바위보 베팅금액 리미트 추가

--- main.py
+++ main.py
@@ -51,6 +51,16 @@ async  def on_message(message):
             embed.add_field(name='돈이 부족합니다!',value=f'{user.name}님의 전재산 : {money}',inline=False)
             await message.channel.send(embed=embed, reference=message)
             return
+        if bet_money == 0:
+            embed = discord.Embed(title="가위바위보", description="", color=0xC19D25)
+            embed.add_field(name='0원을 베팅할수는 없습니다!', value=f'{user.name}님의 전재산 : {money}', inline=False)
+            await message.channel.send(embed=embed, reference=message)
+            return
+        if bet_money > 50001:
+            embed = discord.Embed(title="가위바위보", description="", color=0xC19D25)
+            embed.add_field(name='50000원을 초과해서 베팅할수는 없습니다!', value=f'{user.name}님의 전재산 : {money}', inline=False)
+            await message.channel.send(embed=embed, reference=message)
+            return
 
 
         choices = ['✊', '✌️', '🖐️']
@@ -128,24 +138,6 @@ async  def on_message(message):
         embed.set_image(url=user.display_avatar)
         await message.channel.send(embed=embed,reference=message)
 
-    if message.content == '$?':
-        embed = discord.Embed(title = "명령어 모음",description="모든 명령어는 $로 시작합니다.\n",color =0x9CFF58)
-        embed.add_field(name = '삭제 (number)',value='(number)만큼의 메세지를 삭제합니다.',inline=False)
-        embed.add_field(name="가위바위보 (금액)",value="금액 만큼을 배팅하고 가위바위보를 합니다.",inline=False)
-        embed.add_field(name="내정보", value="본인의 이름, 디스코드 가입일자, 아바타를 확인합니다.", inline=False)
-        embed.add_field(name="?, 명령어", value="명령어 리스트를 확인합니다.", inline=False)
-        embed.add_field(name="등록", value="사용자명, UUID, 돈을 저장할 공간을 만들어줍니다.", inline=False)
-        embed.add_field(name="돈", value="사용자 본인의 돈을 확인합니다.", inline=False)
-        await message.channel.send(embed=embed,reference=message)
-    if message.content == '$명령어':
-        embed = discord.Embed(title = "명령어 모음",description="모든 명령어는 $ 접미사를 사용합니다.\n",color =0x9CFF58)
-        embed.add_field(name = '삭제 (number)',value='(number)만큼의 메세지를 삭제합니다.',inline=False)
-        embed.add_field(name="가위,바위,보",value="가위, 바위 또는 보로 사용합니다.",inline=False)
-        embed.add_field(name="내정보", value="본인의 이름및태그, 디스코드 가입일자, 아바타를 확인합니다.", inline=False)
-        embed.add_field(name="?, 명령어", value="명령어 리스트를 확인합니다.", inline=False)
-        embed.add_field(name="등록", value="사용자명, UUID, 돈을 저장할 공간을 만들어줍니다.", inline=False)
-        await message.channel.send(embed=embed,reference=message)
-
     if message.content == "$등록":
         tz = pytz.timezone('Asia/Seoul')
         now = datetime.now(tz)
@@ -174,7 +166,7 @@ async  def on_message(message):
 
                 return
 
-        # 사용자 정보를 Excel 파일에 추가하기
+        # 사용자 정보를 Excel 파일에 추가
         sheet.append([name, hex(user.id), money, last_checkin])
         wb.save('userDB.xlsx')
         embed = discord.Embed(title="유저 등록", description="", color=0x3AE9E9)
@@ -183,51 +175,52 @@ async  def on_message(message):
         embed.set_footer(text=message.author.name, icon_url=message.author.display_avatar)
         await message.channel.send(embed=embed, reference=message)
 
-    '''if message.content.startswith("$삭제"):
+    if message.content.startswith("$삭제"):
+        role = discord.utils.get(message.guild.roles, name = "봇관리")
+        if role in message.author.roles:
         # 숫자 파싱
-        try:
-            num_messages = int(message.content.split()[1])
-        except (IndexError, ValueError):
-            await message.channel.send("잘못된 입력입니다. $삭제 (number) 형식으로 입력해주세요.",reference = message)
-            return
+            try:
+                num_messages = int(message.content.split()[1])
+            except (IndexError, ValueError):
+                await message.channel.send("잘못된 입력입니다. $삭제 (number) 형식으로 입력해주세요.",reference = message)
+                return
 
-        # 봇이 메시지를 보냄
-        bot_message = await message.channel.send("정말 삭제하시겠습니까? 기록으로 남게될것입니다.\n삭제하실려면 아래 이모지를 10초 이내로 클릭해주세요.",reference = message)
+            # 봇이 메시지를 보냄
+            bot_message = await message.channel.send("정말 삭제하시겠습니까? 기록으로 남게될것입니다.\n삭제하실려면 아래 이모지를 10초 이내로 클릭해주세요.",reference = message)
 
-        # 체크 표시 이모지 추가
-        await bot_message.add_reaction("✅")
+            # 체크 표시 이모지 추가
+            await bot_message.add_reaction("✅")
 
-        # 체크 표시 이모지 대기
-        def check(reaction, user):
-            return user == message.author and str(reaction.emoji) == "✅"
+            # 체크 표시 이모지 대기
+            def check(reaction, user):
+                return user == message.author and str(reaction.emoji) == "✅"
 
-        try:
-            reaction, user = await client.wait_for("reaction_add", timeout=5, check=check)
-        except asyncio.TimeoutError:
-            await message.channel.send("확인 시간이 초과 되었습니다.",reference = message)
+            try:
+                reaction, user = await client.wait_for("reaction_add", timeout=5, check=check)
+            except asyncio.TimeoutError:
+                await message.channel.send("확인 시간이 초과 되었습니다.",reference = message)
+            else:
+                # 메시지 삭제
+                deleted_messages = await message.channel.purge(limit = num_messages + 2)#명령어 + 봇메세지
+                embed = discord.Embed(title="🚧대화내역 삭제🚧", description="", color=0xDF2E2E)
+                embed.add_field(name=f"메세지 {num_messages}개가 삭제되었습니다!! ", value="10초 내로 📜 이모지 반응시 삭제된 메시지 로그를 DM 으로 확인할 수 있습니다.", inline=False)
+                embed.set_footer(text=f"삭제자 : {message.author.name}", icon_url=message.author.display_avatar)
+                log_message = await message.channel.send(embed = embed)
+                await log_message.add_reaction('📜')
+                def check(reaction, user):
+                    return user == message.author and str(reaction.emoji) == '📜'
+                try:
+                    reaction, user = await client.wait_for("reaction_add", timeout = 10, check = check)
+                except asyncio.TimeoutError:
+                    pass
+                else:
+                    #DM으로 로그전송
+                    log_text = '\n'.join([f"{message.author}: {message.content}\n" for message in reversed(deleted_messages)])
+                    await message.author.send(f"요청하신 삭제된 메세지 로그입니다.\n\n{log_text}")
+
         else:
-            # 메시지 삭제
-            await message.channel.purge(limit=num_messages + 2)  # +2 커맨드 메세지 + 봇 메세지 포함
-            embed = discord.Embed(title="🚧대화내역 삭제🚧", description="", color=0xDF2E2E)
-            embed.add_field(name=f"메세지 {num_messages}개가 삭제되었습니다!! ", value="", inline=False)
-            embed.set_footer(text=f"삭제자 : {message.author.name}", icon_url=message.author.display_avatar)
-            await message.channel.send(embed=embed,reference = message)'''
+            await message.channel.send("이 명령어를 실행할 권한이 없습니다.",reference = message)
 
-    if message.content == "$돈":
-        user = message.author
-
-        wb = openpyxl.load_workbook("userDB.xlsx")
-        sheet = wb.active
-
-        money = None
-        # 돈관련 함수
-        for row in sheet.iter_rows(values_only=True):
-            if row[1] == hex(user.id):
-                money = row[2]
-                break
-        embed=discord.Embed(title = '돈', description="",color = 0x07B61E)
-        embed.add_field(name=f'{user.name}님의 돈은 총 {money}원 입니다.',value="",inline=False)
-        await message.channel.send(embed=embed,reference = message)
 
     if message.content == "$출석":
         tz = pytz.timezone('Asia/Seoul')
@@ -279,6 +272,86 @@ async  def on_message(message):
             embed.set_thumbnail(url=message.author.display_avatar)
             embed.set_footer(text=message.author.name, icon_url=message.author.display_avatar)
             await message.channel.send(embed=embed, reference=message)
+
+    if message.content == "$랜덤박스":
+        user = message.author
+
+        wb = openpyxl.load_workbook("userDB.xlsx")
+        sheet = wb.active
+
+        money = None
+        for row in sheet.iter_rows(values_only=True):
+            if row[1] == hex(user.id):
+                money = row[2]
+                break
+        if money < 10000:
+            embed = discord.Embed(title="랜덤박스", description="", color=0xC64DCA)
+            embed.add_field(name='돈이 부족합니다!',value=f'{user.name}님의 전재산 : {money}',inline=False)
+            await message.channel.send(embed=embed, reference=message)
+            return
+        else:
+            for row in sheet.iter_rows():
+                if row[1].value == hex(user.id):
+                    row[2].value -= 10000
+                    wb.save("userDB.xlsx")
+                    break
+            # 숫자 이모지 리스트
+            number_emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
+            # 금액 확률 리스트
+            money_probabilities = [0.4, 0.4, 0.3,0.2, 0.09, 0.07, 0.02, 0.009, 0.001]
+            # 금액 리스트
+            money_list = [0, 1000, 5000, 10000,25000, 35000, 50000, 80000, 100000]
+
+            # 메시지 전송
+            embed = discord.Embed(title="랜덤박스", description="", color=0xC64DCA)
+            embed.add_field(name='이모지 하나를 선택해주세요!', value=f'', inline=False)
+
+            sent_message = await message.channel.send(embed=embed, reference=message)
+
+
+            # 이모지 추가
+            for emoji in number_emojis:
+                await sent_message.add_reaction(emoji)
+
+            # 이모지에 할당된 금액 딕셔너리 초기화
+            emoji_money_dict = {}
+
+            # 각 이모지에 금액 할당
+            for emoji in number_emojis:
+                money = random.choices(money_list, weights=money_probabilities)[0]
+                emoji_money_dict[emoji] = money
+
+            def check(reaction, user):
+                return user == message.author and str(reaction.emoji) in number_emojis
+
+            try:
+                reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+            except asyncio.TimeoutError:
+                await message.channel.send('시간 초과')
+            else:
+                selected_emoji = str(reaction.emoji)
+                selected_money = emoji_money_dict[selected_emoji]
+                for row in sheet.iter_rows():
+                    if row[1].value == hex(user.id):
+                        row[2].value += int(selected_money)
+                        wb.save("userDB.xlsx")
+                        break
+                money = None
+                # 돈관련 함수
+                for row in sheet.iter_rows(values_only=True):
+                    if row[1] == hex(user.id):
+                        money = row[2]
+                        wb.save("userDB.xlsx")
+                        break
+                if selected_money == 0:
+                    embed = discord.Embed(title="랜덤박스", description="", color=0xC64DCA)
+                    embed.add_field(name='꽝입니다..', value=f'{user.name}님의 전재산 : {money}', inline=False)
+                    await message.channel.send(embed=embed, reference=message)
+                else:
+                    embed = discord.Embed(title="랜덤박스", description="", color=0xC64DCA)
+                    embed.add_field(name=f'{selected_money}에 당첨되었습니다!', value=f'{user.name}님의 전재산 : {money}', inline=False)
+                    await message.channel.send(embed=embed, reference=message)
+
 
 
 client.run(token)
