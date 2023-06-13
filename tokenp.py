f4c7d4fea280d731019e3b0e47ecd9d66e2fc4d0
chamc <chamchi0129@gmail.com>
2023-06-13 11:33:47 +0900

    Token

---
+++ tokenp.py
@@ -0,0 +1,11 @@
+def token1():
+    return 'MTEwODYyNDA1NzYzNDY1MjI0MA.Gcqk6p.pdDz-DJnqLt4921IxFB0XqYar6ESmt-fYF68GI'
+
+def googleAPIToken():
+    return 'AIzaSyAQZWafYJ2AMPXSYAB00PHG8CeIsBeJ-5Y'
+
+def NaverClientID():
+    return 'z7qu35Zg4zH0GzwhgRNL'
+
+def NaverClientSecret():
+    return 'ASG3yqEv7o'
