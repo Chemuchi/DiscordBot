023b6041f7077edf2c1a628cbc14628ffb976a56
chamc <chamchi0129@gmail.com>
2023-07-08 22:25:26 +0900

    .

---
+++ cogs/Hangang.py
@@ -0,0 +1,29 @@
+import discord
+import requests
+import json
+
+from discord.ext import commands
+from discord import app_commands
+
+from tokens import guild_id
+
+
+class hangang(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.tree.sync()
+        print(f"{__name__}이 성공적으로 로드됨.")
+
+    @commands.hybrid_command(name="한강", description="코인이라도 떨어지셨나? 호호~")
+    async def hangang(self, ctx: commands.Context):
+        response = requests.get('https://api.hangang.msub.kr/')
+        data = json.loads(response.text)
+        temp = data['temp']
+        await ctx.reply(f"현재 한강수온은 {temp}도 입니다.")
+
+
+async def setup(client):
+    await client.add_cog(hangang(client), guilds=[discord.Object(id=guild_id())])
